f00d8c69d7f2a609a88592fa0211820f8fff1b3a
Arif Foysal Nayeem <wayne11nayem@gmail.com>
2024-09-03 03:00:41 +0600

    nav created

---
+++ partials/__nav.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!--Navbar Fonts  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Playwrite+AT:ital,wght@0,100..400;1,100..400&display=swap" rel="stylesheet">
+
+<style>
+    #branding{
+        font-family: "Caveat", cursive;
+    }
+</style>
+
+</head>
+<body>
+    <nav class="py-4 px-6 bg-teal-950">
+        <section class="text-white max-w-6xl mx-auto flex justify-between items-center">
+            <div id="branding" class="text-5xl font-semibold">Sarwar</div>
+                <div class="hidden sm:flex sm:gap-6 ">
+                    <a href="">Home</a>
+                    <a href="">Work</a>
+                    <a href="">Services</a>
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                </div>
+                <!-- ham menu -->
+                <button class="sm:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+</svg>
+                </button>
+
+        </section>
+    </nav>    
+</body>
+</html>
